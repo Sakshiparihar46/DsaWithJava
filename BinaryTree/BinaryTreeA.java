4decb893124dedb4d76e8af0caf0a4c2734d4921
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-02-12 01:06:11 +0530

    learn post order preorder and inorder of the binary tree

--- BinaryTree/BinaryTreeA.java
+++ BinaryTree/BinaryTreeA.java
@@ -27,11 +27,41 @@ public class BinaryTreeA {
 
             return newNode;
         }
+
+        public static void preorder(Node root){
+            if(root==null){
+                System.out.print("-1 ");
+                return;
+            }
+            System.out.print(root.data+" ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+
+        public static void inorder(Node root){
+            if(root==null){
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data+" ");
+            inorder(root.right);
+
+        }
+
+        public static void postorder(Node root){
+            if(root==null){
+                return;
+            }
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data+" ");
+
+        }
     }
     public static void main(String[] args) {
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree=new BinaryTree();
-       Node root= tree.buildTree(nodes); 
-       System.out.println(root.data);
+        Node root= tree.buildTree(nodes); 
+        tree.postorder(root);
     }
 }
