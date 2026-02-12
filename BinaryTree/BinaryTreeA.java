f978a539e5a59adf65288150e9dcf037d5e36201
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-02-12 12:52:53 +0530

    add level order

--- BinaryTree/BinaryTreeA.java
+++ BinaryTree/BinaryTreeA.java
@@ -1,5 +1,5 @@
 package BinaryTree;
-
+import java.util.*;
 public class BinaryTreeA {
     static class Node{
         int data;
@@ -47,7 +47,7 @@ public class BinaryTreeA {
             inorder(root.right);
 
         }
-
+        
         public static void postorder(Node root){
             if(root==null){
                 return;
@@ -57,11 +57,41 @@ public class BinaryTreeA {
             System.out.print(root.data+" ");
 
         }
+        //Level order
+        public static void levelOrder(Node root){
+            if(root==null){
+                return;
+            }
+            Queue<Node> q=new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while(!q.isEmpty()){
+                Node currNode=q.remove();
+                if(currNode==null){
+                    System.out.println();
+                    if(q.isEmpty()){
+                        break;
+                    }else{
+                        q.add(null);
+                    }
+                }else{
+                System.out.print(currNode.data+" ");
+                if(currNode.left!=null){
+                    q.add(currNode.left);
+                }
+                if(currNode.right!=null){
+                    q.add(currNode.right);
+                }
+            }
+            }
+        }
     }
     public static void main(String[] args) {
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree=new BinaryTree();
         Node root= tree.buildTree(nodes); 
-        tree.postorder(root);
+        // tree.postorder(root);
+        tree.levelOrder(root);
     }
 }
