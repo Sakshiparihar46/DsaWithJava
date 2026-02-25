0bea34f8df2476aecb537986ce6a267866006a0e
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-02-25 22:30:57 +0530

    add lowest common ancestor code

--- BinaryTree/BinaryTreeB.java
+++ BinaryTree/BinaryTreeB.java
@@ -156,6 +156,91 @@ public class BinaryTreeB {
         KLevel(root.right, level+1,k);
 
     }
+
+    public static boolean getPath(Node root,int n,ArrayList<Node> path){
+        if(root==null){
+            return false;
+        }
+        path.add(root);
+        if(root.data==n){
+            return true;
+        }
+        boolean foundLeft=getPath(root.left,n,path);
+        boolean foundRight=getPath(root.right,n,path);
+
+        if(foundLeft|| foundRight){
+            return true;
+        }
+        path.remove(path.size()-1);
+        return false;
+
+    }
+
+    public static Node lca(Node root,int n1,int n2){
+        ArrayList<Node> path1=new ArrayList<>();
+        ArrayList<Node> path2=new ArrayList<>();
+        getPath(root,n1,path1);
+        getPath(root,n2,path2);
+
+        //last common ancestor
+        int i=0;
+        for(i=0;i<path1.size();i++){
+            if(path1.get(i)!=path2.get(i)){
+                break;
+            }
+        }
+
+        //last equal node->i-1th
+        Node lca=path1.get(i-1);
+        return lca;
+    }
+
+    public static Node lca2(Node root,int n1,int n2 ){
+        if(root==null){
+            return null;
+        }
+        if(root==null||root.data==n1 || root.data==n2){
+            return root;
+        }
+
+        Node leftLca=lca2(root.left,n1,n2);
+        Node rightLca=lca2(root.right,n1,n2);
+
+        //leftLca rightLca=null
+        if(rightLca==null){
+            return leftLca;
+        }
+        if(leftLca==null){
+            return rightLca;
+        }
+
+        return root;
+    }
+
+    public static int lcaDist(Node root,int n){
+        if(root==null){
+            return -1;
+        }
+        if(root.data==n){
+            return 0;
+        }
+        int leftDist=lcaDist(root.left,n);
+        int rightDist=lcaDist(root.right,n);
+
+        if(leftDist==-1 &&rightDist==-1){
+            return -1;
+        }else if(leftDist==-1){
+            return rightDist+1;
+        }else{
+            return leftDist+1;
+        }
+    }
+
+    public static int minDist(Node root,int n1,int n2){
+       Node lca=lca2(root,n1,n2); 
+       int dist1=lcaDist(root,n1);
+       int dist2=lcaDist(root,n2);
+    }
     public static void main(String[] args) {
 
         //tree
@@ -167,7 +252,6 @@ public class BinaryTreeB {
         root.right.left=new Node(6);
         root.right.right=new Node(7);
 
-        int k=2;
         //subtree
         // Node subRoot=new Node(2);
         // subRoot.left=new Node(4);
@@ -177,10 +261,12 @@ public class BinaryTreeB {
         // System.out.println(sum(root));
         // System.out.println(diameter2(root).ht); 
         // System.out.println(isSubtree(root, subRoot));
-
-
+        
+        // int k=2;
         // topView(root);
+        // KLevel(root,1, k);
 
-        KLevel(root,1, k);
+        int n1=4,n2=7;
+        System.out.println(lca2(root, n1, n2).data);
     }
 }
