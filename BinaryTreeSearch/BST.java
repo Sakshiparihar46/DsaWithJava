b7330b1393ee217cb9124cab9c11d5169624c264
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-03-02 23:13:37 +0530

    add delete function in it

--- BinaryTreeSearch/BST.java
+++ BinaryTreeSearch/BST.java
@@ -35,14 +35,70 @@ public class BST {
         System.out.print(root.data+" ");
         inorder(root.right);
     }
+    public static boolean search(Node root,int key){
+        if(root==null){
+            return false;
+        }
+        if(root.data>key){
+          return search(root.left,key);
+        }else if(root.data<key){
+           return search(root.right,key);
+        }else{
+            return true;
+        }
+    }
+
+    public static Node findInorderSuccessor(Node root){
+          while(root.left!=null){
+            root=root.left;
+          }
+          return root;
+    }
+
+    public static Node delete(Node root,int val){
+        if(root.data<val){
+            root.right=delete(root.right,val);
+        }
+        else if(root.data>val){
+            root.left=delete(root.left,val);
+        }
+        else{
+            //case 1-leaf  node
+            if(root.left==null && root.right==null){
+                return null;
+            }
+            //case2-single child
+            if(root.left==null){
+                return root.right;
+            }else if(root.right==null){
+                return root.left;
+            }
+            //case3-both children
+            Node IS=findInorderSuccessor(root.right);
+            root.data=IS.data;
+            root.right=delete(root.right,IS.data);   
+        }
+        return root;
+    }
 
     public static void main(String[] args) {
-        int values[]={5,1,3,4,2,7};
+        int values[]={8, 5, 3, 1, 4, 6, 10, 11, 14};
         Node root=null;
 
         for(int i=0;i<values.length;i++){
             root=insert(root,values[i]);
         }
+        inorder(root);
+        System.out.println();
+
+        // if(search(root,10 )){
+        //     System.out.println("found");
+        // }else{
+        //     System.out.println("not found");
+        // }
+
+        delete(root,1);
+        System.out.println();
         inorder(root);
     }
 }
