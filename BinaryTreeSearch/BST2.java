8c5549894fd0f13f559e996565628b01983a3e16
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-03-03 13:54:42 +0530

    add code

--- BinaryTreeSearch/BST2.java
+++ BinaryTreeSearch/BST2.java
@@ -12,6 +12,26 @@ public class BST2 {
         }
     }
 
+    public static Node createMirror(Node root){
+        if(root==null){
+            return null;
+        }
+        Node leftMirror=createMirror(root.left);
+        Node rightMirror=createMirror(root.right);
+
+        root.left=rightMirror;
+        root.right=leftMirror;
+         return root;
+    }
+     
+    public static void preorder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
     public static void main(String[] args) {
         Node root=new Node(8);
         root.left=new Node(5);
@@ -19,5 +39,8 @@ public class BST2 {
         root.left.left=new Node(3);
         root.left.right=new Node(6);
         root.right.right=new Node(11);
+
+        root=createMirror(root);
+        preorder(root);
     }
 }
