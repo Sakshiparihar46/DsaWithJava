6f718a3c899b4a79f716c04b3dd7b5e4885f57b8
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-03-14 22:08:30 +0530

    add priority queue function

---
+++ PriorityQueue/priorityQueue.java
@@ -0,0 +1,17 @@
+package PriorityQueue;
+import java.util.*;
+public class priorityQueue {
+    public static void main(String args[]){
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+
+        pq.add(3);
+        pq.add(4);
+        pq.add(1);
+        pq.add(7);
+
+        while(!pq.isEmpty()){
+            System.out.print(pq.peek()+" ");
+            pq.remove();
+        }
+    }
+}
