6474c5ad147dfd1a713e06da20b54a0812bba751
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-03-17 21:56:53 +0530

    add remove function

--- PriorityQueue/Heap1.java
+++ PriorityQueue/Heap1.java
@@ -21,6 +21,44 @@ public class Heap1 {
         public int peek(){
             return arr.get(0);
         }
+
+        private void heapify(int i){
+            int left=2*i+1;
+            int right=2*i+2;
+            int minIndx=i;
+
+            if(left<arr.size() && arr.get(minIndx)>arr.get(left)){
+                minIndx=left;
+            }
+
+            if(right<arr.size() && arr.get(minIndx)>arr.get(right)){
+                minIndx=right;
+            }
+
+            if(minIndx!=i){
+                //swap
+                int temp=arr.get(i);
+                arr.set(i, arr.get(minIndx));
+                arr.set(minIndx, temp);
+
+                heapify(minIndx);
+            }
+        }
+        public int remove(){
+            int data=arr.get(0);
+
+            //step1--swap first and last
+            int temp=arr.get(0);
+            arr.set(0, arr.size()-1);
+            arr.set(arr.size()-1, temp);
+
+            //step2-delete last
+            arr.remove(arr.size()-1);
+
+            //step3--heapify
+            heapify(0);
+            return data;
+        }
     }
 
     public static void main(String[] args) {
