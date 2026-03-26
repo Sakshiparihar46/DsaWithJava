12bcf3142dd4456e714db027da4ae71a34719e68
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-03-26 13:42:45 +0530

    add hashmap

---
+++ Hashing/hashmap1.java
@@ -0,0 +1,15 @@
+package Hashing;
+import java.util.*;
+public class hashmap1{
+    public static void main(String args[]){
+        //create
+        HashMap<String,Integer> hm=new HashMap<>();
+
+        //insert
+        hm.put("Tea",20);
+        hm.put("burger",70);
+
+        System.out.println(hm);
+
+    }
+}
