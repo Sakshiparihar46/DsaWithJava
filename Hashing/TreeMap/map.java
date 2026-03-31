390c50365bc386e4257f82aed97b8eb9dce967b9
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-03-31 13:53:49 +0530

    add treeMap

---
+++ Hashing/TreeMap/map.java
@@ -0,0 +1,15 @@
+package Hashing.TreeMap;
+import java.util.TreeMap;
+
+public class map {
+     public static void main(String[] args) {
+        TreeMap<String,Integer> tm=new TreeMap<>();
+        tm.put("china",100);
+        tm.put("india",120);
+        tm.put("bhutan",23);
+        tm.put("nepal",50);
+
+        System.out.println(tm);
+
+    }
+}
