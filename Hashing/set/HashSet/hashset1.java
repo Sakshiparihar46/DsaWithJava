0e769b0e460b04341a57dab3da1177ded381dac2
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-04-01 22:30:03 +0530

    add Hashset

--- Hashing/set/HashSet/hashset1.java
+++ Hashing/set/HashSet/hashset1.java
@@ -1,7 +1,15 @@
 package Hashing.set.HashSet;
 
+import java.util.HashSet;
+
 public class hashset1 {
     public static void main(String[] args) {
-        
+        HashSet<Integer> set=new HashSet<>();
+        set.add(2);
+        set.add(4);
+        set.add(2);
+        set.add(1);
+
+        System.out.println(set);
     }
 }
