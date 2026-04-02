6c018468229eeebb6a55ab2e26488d1ff82bf52f
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-04-02 22:21:39 +0530

    add treeset

---
+++ Hashing/set/TreeSet/treeset1.java
@@ -0,0 +1,16 @@
+package Hashing.set.TreeSet;
+
+import java.util.TreeSet;
+
+public class treeset1 {
+    public static void main(String[] args) {
+        TreeSet<String> ts = new TreeSet<>();
+        ts.add("delhi");
+        ts.add("mumbai");
+        ts.add("noida");
+        ts.add("bengaluru");
+
+        System.out.println(ts);
+
+    }
+}
