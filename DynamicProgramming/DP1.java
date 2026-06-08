3d0083a2b8eecff9c9345e5badf0b4539cb25101
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-06-08 20:15:56 +0530

    add tabulated code for fibonacci

--- DynamicProgramming/DP1.java
+++ DynamicProgramming/DP1.java
@@ -1,6 +1,8 @@
 package DynamicProgramming;
 
 public class DP1 {
+
+    //memoization
     public static int fibo(int n,int f[]){
         if(n==0 || n==1){
             return n;
@@ -14,9 +16,22 @@ public class DP1 {
         return f[n];
     }
 
+    //tabulation
+    public static int fib(int n){
+        int dp[]=new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
+    }
+
     public static void main(String[] args) {
-        int n=5;
+        int n=6;
         int f[]=new int[n+1];
-        System.out.println(fibo(n, f));
+        System.out.println(fib(n));
     }
 }
