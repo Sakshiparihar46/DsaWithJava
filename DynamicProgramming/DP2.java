b63b516a18bb518742e2d3369a9d0e433a56d925
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-06-09 20:34:39 +0530

    solve this question by memoization

--- DynamicProgramming/DP2.java
+++ DynamicProgramming/DP2.java
@@ -1,6 +1,7 @@
 package DynamicProgramming;
-
+import java.util.*;
 public class DP2 {
+    //recursion
     public static int ways(int n){
         if(n==0){
             return 1;
@@ -10,8 +11,29 @@ public class DP2 {
         }
         return ways(n-1)+ways(n-2);
     }
+
+    //dynamic programming(memoization)
+    public static int ways2(int n,int w[]){
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+            return 0;
+        }
+        if(w[n]==-1){
+            w[n]= ways2(n-1,w)+ways2(n-2,w);
+        }
+
+        return w[n];
+    } 
+
     public static void main(String args[]){
         int n=5;
-        System.out.println(ways(n));
+        // System.out.println(ways(n));
+
+
+        int ways[]=new int [n+1];
+        Arrays.fill(ways,-1);
+        System.out.println(ways2(n, ways));
     }
 }
