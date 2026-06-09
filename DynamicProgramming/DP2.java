f6caab782e6a79c12d411c2aed4bfbde3553880b
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-06-09 20:15:59 +0530

    solve this question first by recursion

---
+++ DynamicProgramming/DP2.java
@@ -0,0 +1,17 @@
+package DynamicProgramming;
+
+public class DP2 {
+    public static int ways(int n){
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+            return 0;
+        }
+        return ways(n-1)+ways(n-2);
+    }
+    public static void main(String args[]){
+        int n=5;
+        System.out.println(ways(n));
+    }
+}
