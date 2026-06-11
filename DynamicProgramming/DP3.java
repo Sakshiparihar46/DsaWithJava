b7dac8c6b3ee86a61ce363b20c3dbf12e517ad83
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-06-11 14:29:04 +0530

    solve by tabulation

--- DynamicProgramming/DP3.java
+++ DynamicProgramming/DP3.java
@@ -41,6 +41,33 @@ public class DP3 {
 
     }
 
+    //tabulation
+    public static int knapsack3(int val[],int wt[],int W){
+        int n=val.length;
+        int dp[][]=new int[n+1][W+1];
+        for(int i=0;i<dp.length;i++){//0th col
+            dp[i][0]=0;
+        }
+        for (int j = 0; j < dp[0].length; j++) {//0th row
+            dp[0][j]=0;
+        }
+
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 1; j < W+1; j++) {
+                int v=val[i-1];
+                int w=wt[i-1];
+                if(w<=j){//valid
+                    int incProfit=v+dp[i-1][j-w];
+                    int expProfit=dp[i-1][j];
+                    dp[i][j]=Math.max(incProfit,expProfit);
+                }else{//invalid
+                    int expProfit=dp[i-1][j];
+                    dp[i][j]=expProfit;
+                }
+            }
+        }
+        return dp[n][W];
+    }
 
     public static void main(String[] args) {
         int val[]={15,14,10,45,30};
@@ -55,6 +82,8 @@ public class DP3 {
 
         // System.out.println(knapsack(val, wt, W, val.length));
 
-        System.out.println(knapsack2(val, wt, W,val.length,dp));
+        // System.out.println(knapsack2(val, wt, W,val.length,dp));
+
+        System.out.println(knapsack3(val, wt, W));
     }
 }
