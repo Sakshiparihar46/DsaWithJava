41337ee38cc24eb0e56cd0040646e7fcac555e5e
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-06-15 20:21:50 +0530

    add some comments for better understanding

--- DynamicProgramming/DP8.java
+++ DynamicProgramming/DP8.java
@@ -5,9 +5,9 @@ public class DP8 {
         if(n==0 || m==0){
             return 0;
         }
-        if(str1.charAt(n-1)==str2.charAt(m-1)){
+        if(str1.charAt(n-1)==str2.charAt(m-1)){//same
             return lcs(str1,str2,n-1,m-1)+1;
-        }else{
+        }else{//for in different cases
             int ans1=lcs(str1, str2,n-1, m);
             int ans2=lcs(str1, str2,n, m-1);
             return Math.max(ans1, ans2);
