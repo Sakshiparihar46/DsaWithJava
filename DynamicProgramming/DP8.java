33bf8681c3971018e4566f0d30265fc77828bab4
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-06-16 13:29:42 +0530

    solve by tabulation

--- DynamicProgramming/DP8.java
+++ DynamicProgramming/DP8.java
@@ -34,6 +34,22 @@ public class DP8 {
         }
     }
 
+    //tabulation
+    public static int lc3(String str1,String str2,int n,int m){
+        int dp[][]=new int[n+1][m+1];
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 1; j < m+1; j++) {
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }else{
+                    int ans1=dp[i-1][j];
+                    int ans2=dp[i][j-1];
+                    dp[i][j]=Math.max(ans1,ans2);
+                }
+            }
+        }
+        return dp[n][m];
+    }
 
     public static void main(String[] args) {
         String str1="abcdge";
@@ -52,6 +68,7 @@ public class DP8 {
             }
         }
         System.out.println(lcs1(str1, str2, n, m, dp));
+        System.out.println(lc3(str1, str2, n, m));
 
     }
 }
