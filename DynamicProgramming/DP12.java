56808090d6f254bf2e9e9ad17d98ae8de61452a8
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-06-18 00:37:55 +0530

    solve String conversion

---
+++ DynamicProgramming/DP12.java
@@ -0,0 +1,37 @@
+package DynamicProgramming;
+
+public class DP12 {
+      public static int lcs(String str1,String str2){
+        int n=str1.length();
+        int m=str2.length();
+        int dp[][]=new int[n+1][m+1];
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 1; j < m+1; j++) {
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }else{
+                    int ans1=dp[i-1][j];
+                    int ans2=dp[i][j-1];
+                    dp[i][j]=Math.max(ans1,ans2);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+     public static int stringConversion(String str1,String str2,int n,int m){
+        //show delete operation
+       int ans1=n-lcs(str1, str2);
+       //show add operation
+       int ans2=m-lcs(str1,str2);
+       return ans1+ans2;
+    }
+
+
+    public static void main(String[] args) {
+        String str1="pear";
+        String str2="sea";
+        System.out.println(stringConversion(str1, str2, str1.length(), str2.length()));
+
+    }
+}
