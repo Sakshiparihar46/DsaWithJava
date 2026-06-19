5b9fc6fe54893b26478bb96e8369faf3b7dd60d9
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-06-19 16:54:44 +0530

    sove this qestion by tabulation

--- DynamicProgramming/DP14.java
+++ DynamicProgramming/DP14.java
@@ -18,10 +18,27 @@ public class DP14 {
         arr[n]=ans;
         return arr[n];
     }
+
+    //tabulation
+    public static int catalanTab2(int n){
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+
+        for (int i = 2; i <n+1;i++) {
+            for(int j=0;j<i;j++){
+                dp[i]+=dp[j]*dp[i-j-1];
+            }
+        }
+        return dp[n];
+    }
     public static void main(String[] args) {
         int n=4;
-        int arr[]=new int[n+1];
-        Arrays.fill(arr, -1);
-        System.out.println(catalanRec(n,arr));
+        // int dp[]=new int[n+1];
+        // Arrays.fill(dp, -1);
+        // System.out.println(catalanRec(n,dp));
+
+        System.out.println(catalanTab2(n));
+
     }   
 }
