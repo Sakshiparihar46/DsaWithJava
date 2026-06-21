6ebeeb629d06be9d74f34e6e295c9d27da24a111
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-06-21 14:25:00 +0530

    add tabulation code for solving mcm

--- DynamicProgramming/DP17.java
+++ DynamicProgramming/DP17.java
@@ -36,13 +36,38 @@ public class DP17 {
         dp[i][j]=ans;
         return dp[i][j];
     }
+    //tabulation
+    public static int mcm3(int arr[]){
+        int n=arr.length;
+        int dp[][]=new int [n][n];
+        for(int i=0;i<arr.length;i++){
+            dp[i][i]=0;
+        }
+
+        for (int len = 2; len <=n-1; len++) {
+            for (int i=1;i<=n-len;i++){
+                int j=i+len-1;
+                dp[i][j]=Integer.MAX_VALUE;
+                for (int k = i; k <=j-1; k++) {
+                    int cost1=dp[i][k];
+                    int cost2=dp[k+1][j];
+                    int cost3=arr[i-1]*arr[k]*arr[j];
+                    dp[i][j]=Math.min(cost1+cost2+cost3,dp[i][j]);
+                }
+            }
+        }
+        return dp[1][n-1];
+    }
+
     public static void main(String[] args) {
         int arr[]={1,2,3,4,3};
         // System.out.println(mcm(arr, 1, arr.length-1));
-        int dp[][]=new int[arr.length][arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            Arrays.fill(dp[i],-1);
-        }
-        System.out.println(mcm1(arr, 1, arr.length-1,dp));
+        // int dp[][]=new int[arr.length][arr.length];
+        // for (int i = 0; i < arr.length; i++) {
+        //     Arrays.fill(dp[i],-1);
+        // }
+        // System.out.println(mcm1(arr, 1, arr.length-1,dp));
+
+        System.out.println(mcm3(arr));
     }
 }
